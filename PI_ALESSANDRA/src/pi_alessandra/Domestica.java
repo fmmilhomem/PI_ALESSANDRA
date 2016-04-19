f3d150e38b4d6532a93e7a1eff21ad2f9a0cc3de
felipe.cfischer <felipe.cfischer@CAS521379W07B11.senacsp.edu.br>
2016-04-19 20:14:34 -0300

    Alterado if para while, na validação da opção escolhida

--- PI_ALESSANDRA/src/pi_alessandra/Domestica.java
+++ PI_ALESSANDRA/src/pi_alessandra/Domestica.java
@@ -9,7 +9,7 @@ public class Domestica {
     public static void main(String[] args) {
         boolean opcaoMenu = false;
         int aux;
-        
+
         do {
             System.out.println(" 1 - Lêr detalhes da vaga");
             System.out.println(" 2 - Visualizar endereço para serviço");
@@ -18,21 +18,23 @@ public class Domestica {
             System.out.println("Informe a opção do menu:");
             aux = entrada.nextInt();
 
-            if ((aux == 1) || (aux == 2)) {
-                opcaoMenu = true;
-            } else {
-                // Se numero digitado estiver fora do range retorna a msg e pede p digitar novamente
-                System.out.println("Opção inválida, tente novamente");
+            //Validando se opção escolhida existe no menu
+            while ((aux != 1) && (aux != 2)) {
+                //Se a opção escolhida não tive no menu, a mensagem pede para digitar novamente
+                System.out.println("Opção inválida, tente novamente: ");
                 aux = entrada.nextInt();
-            }   // Se numero digitado estiver no range cai no switch
+            }
+            opcaoMenu = true; //Recebe valor verdadeiro após passar pela validação
+
         } while (opcaoMenu == false);
+        
+        //Caso 1 e 2 do menu
         switch (aux) {
             case 1:
                 System.out.println("Lêr detalhes da vaga");
                 break;
-            case 2:
+            default:
                 System.out.println("Visualizar endereço para serviço");
-                break;
         }
     }
 }
