f00d0e10c88eafb1631b0a8223f9c69100b26af9
Felipe Fischer <felipecorreiafischer@hotmail.com>
2016-04-30 18:12:49 -0300

    Alterado

--- PI_ALESSANDRA/src/pi_alessandra/Domestica.java
+++ PI_ALESSANDRA/src/pi_alessandra/Domestica.java
@@ -4,59 +4,96 @@ import java.util.Scanner;
 
 public class Domestica {
 
+    static int aux;
+    static String vetorLocal[] = new String[1];
+    static double vetorAuxNumero[] = new double[2];
+
     static Scanner entrada = new Scanner(System.in);
+
+    static void mostrarMenu() {
+        System.out.println();
+        System.out.println(" 1 - Cadastro de vagas:");
+        System.out.println(" 2 - Relatório");
+        System.out.println(" 3 - Lêr detalhes da vaga");
+        System.out.println(" 4 - Aceitar a vaga");// precisa elaborar algoritmo
+        System.out.println(" 5 - Sair "); //precisa elaborar algoritmo
+    }
+
+    static String pesquisaVaga(String localVaga) {
+        return localVaga;
+    }
+
+    static void switchCase1() {
+
+        // aqui entra o comando para cadastrar a vaga
+        System.out.println("Quantos dias da semana precisará ser trabalhado?");//int= diasdeTrabalho;
+        vetorAuxNumero[0] = entrada.nextDouble();
+
+        System.out.println("Quanto deseja pagar para a diarista?"); //double=valorDiarista;
+        vetorAuxNumero[1] = entrada.nextDouble();
+
+        System.out.println("Qual a localidade?");
+        vetorLocal[0] = entrada.nextLine();
+
+    }
+
+    static void metodoPrintf() {
+        System.out.printf("\nA vaga está localizada em  " + vetorLocal[0]);
+        System.out.printf("\nA vaga é para trabalhar  dias da semana " + vetorAuxNumero[0]);
+        System.out.printf("\nO salário da empregada será de  " + vetorAuxNumero[1]);
+
+    }
 
     public static void main(String[] args) {
         boolean opcaoMenu = false;
-        int aux;
+        
 
         // Apresentação 
-        System.out.println(" -- Vagas -- ");
-        System.out.println(" 1 - Lista de vagas diárias ");
-        System.out.println(" 2 - Sair ");
+        mostrarMenu();
 
         // Informar opção do menu acima
         System.out.print("Informe a opção do menu: ");
         aux = entrada.nextInt();
-        
+
         // Verifica se a opção menu inicial é válida
-        while ((aux != 1) && (aux != 2)) {
+        // 5 = numero maximo de opcoes do menu
+        while ((aux > 5) || (aux < 1)) {
             System.out.println("Opção inválida, tente novamente: ");
             aux = entrada.nextInt();
         }
-        
-        // Se var for igual a 1 faça o seguinte abaixo senão finaliza o programa
-        if (aux == 1) {
-            // Validando submenu
-            do {
-                System.out.println();
-                System.out.println(" 1 - Lêr detalhes da vaga");
-                System.out.println(" 2 - Visualizar endereço para serviço");
+//Array - jasouCadastrado[i].length
+        //Caso do menu
+        switch (aux) {
+            case 1:
+                System.out.println("Cadastro de vagas:");
+                switchCase1();
+                // metodoPrintf();
 
-                // Informa o que deseja do submenu
-                System.out.print("Informe a opção do menu: ");
-                aux = entrada.nextInt();
+                break;
+            case 2:
+                System.out.println("Relatório");
+                //aqui entra o comando para gerar relatórios
+                // COMO FAZER? QUE TIPO DE RELATORIO ?
+                break;
+            case 3:
+                System.out.println("Lêr detalhes da vaga");
+                //3.1 : Pesquisar a vaga 
+                //criar case 1 e jogar aqui com localVaga
+                String auxLocal;
+                auxLocal = vetorLocal[0];
 
-                // Validando se opção escolhida existe no submenu
-                while ((aux != 1) && (aux != 2)) {
-                    // Se a opção escolhida não tive no submenu, a mensagem pede para digitar novamente
-                    System.out.println("Opção inválida, tente novamente: ");
-                    aux = entrada.nextInt();
-                }
-                opcaoMenu = true; //Recebe valor verdadeiro após passar pela validação
+                break;
 
-            } while (opcaoMenu == false);
+            case 4:
+                System.out.println("Aceitar a vaga:");
+                //4.1 : pesqisar a vaga 
+                // procurar por localVaga case 1
+                break;
+            case 5:
+                //Criar metodo para sair
+                break;
 
-            //Caso 1 e 2 do submenu
-            switch (aux) {
-                case 1:
-                    System.out.println("Lêr detalhes da vaga");
-                    break;
-                default:
-                    System.out.println("Visualizar endereço para serviço");
-            }
         }
-        System.out.println();
         System.out.println("Obrigado volte sempre!"); // Finalizando programa
     }
 }
