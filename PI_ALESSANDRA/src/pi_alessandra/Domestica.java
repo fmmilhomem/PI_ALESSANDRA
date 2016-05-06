438724545edb9b7f9e51e6ae27b54684fa2621b4
Felipe Fischer <felipecorreiafischer@hotmail.com>
2016-05-06 11:54:09 -0300

    Organizado variaveis, alterado nome dos metodos e ajustados as posições.

--- PI_ALESSANDRA/src/pi_alessandra/Domestica.java
+++ PI_ALESSANDRA/src/pi_alessandra/Domestica.java
@@ -3,45 +3,8 @@ package pi_alessandra;
 import java.util.Scanner;
 
 public class Domestica {
-
-    /**
-     * Este metodo exibe o menu principal
-     */
-    static void mostrarMenu() {
-        System.out.printf("\n---------- Menu -------------\n\n");
-        System.out.println(" 1 - Cadastro de vagas:");
-        System.out.println(" 2 - Relatório");
-        System.out.println(" 3 - Lêr detalhes da vaga");
-        System.out.println(" 4 - Aceitar a vaga");// precisa elaborar algoritmo
-        System.out.println(" 5 - Sair "); //precisa elaborar algoritmo
-    }
-
-    static String pesquisaVaga(String localVaga) {
-        return localVaga;
-    }
-
-    static void switchCase1() {
-
-        // aqui entra o comando para cadastrar a vaga
-        i++;
-        System.out.println("Qual a localidade?");
-        vetorLocal[i] = entrada.next();
-        System.out.println("Quantos dias da semana precisará ser trabalhado?");//int= diasdeTrabalho;
-        vetorAuxDiaria[i] = entrada.nextInt();
-        System.out.println("Quanto deseja pagar para a diarista?"); //double=valorDiarista;
-        vetorAuxValor[i] = entrada.nextDouble();
-        System.out.printf("\n--------- Cadastrado seguinte informações ------------\n\n");
-
-    }
-
-    static void metodoPrintf() {
-        System.out.println("A vaga está localizada em  " + vetorLocal[i]);
-        System.out.println("A vaga é para trabalhar " + vetorAuxDiaria[i] + " dias da semana ");
-        System.out.println("O salário da empregada será de  " + vetorAuxValor[i]);
-
-    }
-
-    // Variavel para ler entrada do usuarios 
+    
+      // Variavel para ler entrada do usuarios 
     static Scanner entrada = new Scanner(System.in);
 
     // Variaveis
@@ -51,8 +14,56 @@ public class Domestica {
     static int vetorAuxDiaria[] = new int[10];
     static double vetorAuxValor[] = new double[10];
 
+/**
+ * Este metodo exibe o Menu Principal
+ */
+    static void mostrarMenu() {
+        System.out.printf("\n---------- Menu -------------\n\n");
+        System.out.println(" 1 - Cadastro de vagas:");
+        System.out.println(" 2 - Relatório");
+        System.out.println(" 3 - Lêr detalhes da vaga");
+        System.out.println(" 4 - Aceitar a vaga");// precisa elaborar algoritmo
+        System.out.println(" 5 - Sair "); //precisa elaborar algoritmo
+    }
+    
+/**
+ * Este metodo faz a pesquisar na vaga
+ * @param localVaga
+ * @return 
+ */
+    static String pesquisarVaga(String localVaga) {
+        return localVaga;
+    }
+    
+/**
+ * Este metodo executa no caso 1 (Switch case)
+ */
+    static void executarCaso1() {
+        // aqui entra o comando para cadastrar a vaga
+        i++;
+//        System.out.println("Qual a localidade?");
+//        vetorLocal = entrada.nextLine();
+        System.out.println("Quantos dias da semana precisará ser trabalhado?");//int= diasdeTrabalho;
+        vetorAuxDiaria[i] = entrada.nextInt();
+        System.out.println("Quanto deseja pagar para a diarista?"); //double=valorDiarista;
+        vetorAuxValor[i] = entrada.nextDouble();
+        System.out.printf("\n--------- Cadastrado seguinte informações ------------\n\n");
+
+    }
+
+    /**
+     * Este metodo informa oque foi cadastrado
+     */
+    static void InformarCadastro() {
+        System.out.println("A vaga está localizada em  " + vetorLocal[i]);
+        System.out.println("A vaga é para trabalhar " + vetorAuxDiaria[i] + " dias da semana ");
+        System.out.println("O salário da empregada será de  " + vetorAuxValor[i]);
+
+    }
+
     //------------------------- Execução do programa -------------------------//
     public static void main(String[] args) {
+        String teste;
         boolean validarMenu = false;
 
         while (validarMenu == false) {
@@ -73,9 +84,11 @@ public class Domestica {
             switch (aux) {
                 case 1:
                     System.out.printf("\n----------- Cadastro de vagas -----------\n\n");
-                        i++;
-                        switchCase1();
-                        metodoPrintf();
+                    i++;
+                    System.out.println("Qual a localidade?");
+                    teste = entrada.nextLine();
+                    executarCaso1();
+                    InformarCadastro();
                     break;
 
                 case 2:
