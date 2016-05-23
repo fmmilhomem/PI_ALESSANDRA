4db26b3693af21db80cbd7936635b3b678fd3e88
Felipe Milhomem <felipe.milhomem@vidishopper.com>
2016-05-23 15:47:40 -0300

    Alteração do menu + criação do case 5 .

---
+++ PI_ALESSANDRA/src/pi_alessandra/Vagas.java
@@ -0,0 +1,8 @@
+package pi_alessandra;
+public class Vagas {
+    public String localVagas;
+    public double salarioDiaristas;
+    public byte diasTrabalhados;
+    public String detalhesVagas;
+    public boolean statusVagas = false;
+}
