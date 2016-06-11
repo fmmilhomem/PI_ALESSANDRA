60dd2ee8f8e3f0f157681680af8c457650463969
cfischer <cfischer@192.168.1.108>
2016-06-11 18:04:21 -0300

    Ajustado aceitar vaga, recebe o proximo valor do array.

--- PI_ALESSANDRA/src/pi_alessandra/AceitarVaga.java
+++ PI_ALESSANDRA/src/pi_alessandra/AceitarVaga.java
@@ -3,36 +3,37 @@ package pi_alessandra;
 public class AceitarVaga {
 
     static void aceitarVaga() {
-        boolean vagaSelecionada = false;
-        int auxSelecionar;
-        auxSelecionar = Pesquisar.validarCaseId();
-        Pesquisar.psqId(auxSelecionar);
-        
-        do {
-            for (int i = 0; i < Domestica.listaVagas.length; i++) {
-                auxSelecionar = Integer.parseInt(Domestica.opcaoTeclado);
-
-                if (Domestica.auxiliarTeclado == Domestica.listaVagas[i].id) {
-                    Domestica.listaVagas[i].id = 0;
-                }
-            }
-        } while ((vagaSelecionada == false) || Domestica.auxiliarTeclado == 0);
     }
-    
+
     static void aceitarVagaPsq() {
         boolean vagaSelecionada = false;
-        int auxSelecionar;
-        auxSelecionar = Pesquisar.validarCaseId();
-        Pesquisar.psqId(auxSelecionar);
-        
-        do {
-            for (int i = 0; i < Domestica.listaVagas.length; i++) {
-                auxSelecionar = Integer.parseInt(Domestica.opcaoTeclado);
+        String auxString;
 
-                if (Domestica.auxiliarTeclado == Domestica.listaVagas[i].id) {
-                    Domestica.listaVagas[i].id = 0;
+        System.out.println("Deseja aceitar a vaga? (S / N)");
+        while (vagaSelecionada == false) {
+            //LIMPANDO ENTRADA SCANNER
+            Domestica.entrada.nextLine();
+            auxString = Domestica.entrada.next();
+            //VERIFICA SE VAGA FOI ACEITA OU NÃO
+            //SE FOI SUBSTITUI O VALOR PELO PROXIMO
+            if ("s".equalsIgnoreCase(auxString)) {
+                System.out.println("A vaga foi aceita!");
+                vagaSelecionada = true;
+                Domestica.listaVagas[Pesquisar.vagaSelecionada] = null;
+                for (int i = Pesquisar.vagaSelecionada; i < Domestica.listaVagas.length; i++) {
+                    if (Domestica.listaVagas[i + 1] == null) {
+                        i = Domestica.listaVagas.length;                        
+                    } else {
+                        Domestica.listaVagas[i] = Domestica.listaVagas[i + 1];
+                        Domestica.listaVagas[i + 1] = null;
+                    }
                 }
+            } else if ("n".equalsIgnoreCase(auxString)) {
+                System.out.println();
+                vagaSelecionada = true;
+            } else {
+                System.out.println("Valor inválido (S)sim / (N)não!");
             }
-        } while ((vagaSelecionada == false) || Domestica.auxiliarTeclado == 0);
+        }
     }
 }
